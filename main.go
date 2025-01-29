06ca15042a8f4fc52cd79894412f905ff8f1501c
Conor <conor@johngregg.org>
2025-01-29 15:27:45 -0600

    REdon

--- main.go
+++ main.go
@@ -1,15 +1,38 @@
 package main
 
 import (
-	"fmt"
-	"log"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"html/template"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello, Balls!")
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+
+	indexContent := template.Must(template.ParseFiles("index.gohtml"))
+
+	e.File("/banner.jpg", "banner.jpg")
+
+	e.GET("/", func(c echo.Context) error {
+		return indexContent.Execute(c.Response().Writer, "home")
 	})
 
-	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
+	e.GET("/home", func(c echo.Context) error {
+		if c.Request().Header.Get("HX-Request") == "" {
+			return indexContent.Execute(c.Response().Writer, "home")
+		}
+		return c.String(http.StatusOK, "Home")
+	})
+
+	e.GET("/projects", func(c echo.Context) error {
+		if c.Request().Header.Get("HX-Request") == "" {
+			return indexContent.Execute(c.Response().Writer, "projects")
+		}
+		return c.String(http.StatusOK, "Projects")
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
